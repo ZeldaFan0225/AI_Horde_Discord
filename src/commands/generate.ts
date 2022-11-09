157d4aca340afbef355d1678dfa471ace2e2fcbf
ZeldaFan0225 <robin.butsch@gmail.com>
2022-11-09 14:17:52 +0100

    improve models autocomplete

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -485,7 +485,7 @@ ETA: <t:${Math.floor(Date.now()/1000)+(status?.wait_time ?? 0)}:R>`
                 const models = await context.stable_horde_manager.getModels()
                 if(context.client.config.dev) console.log(models)
                 const available = [{name: "Any Model", value: "YOLO"}, ...models.sort((a, b) => b.performance!-a.performance!).map(m => ({name: `${m.name} | Workers: ${m.count} | Performance: ${m.performance} | Queued: ${m.queued}`, value: m.name!}))].filter(v => !context.client.config.blacklisted_models?.includes(v.value)).filter(v => !option.value || v.name.toLowerCase().includes(option.value.toLowerCase()))
-                return await context.interaction.respond(available.filter(o => o.name.includes(option.value)).slice(0,25))
+                return await context.interaction.respond(available.filter(o => o.name?.toLowerCase().includes(option.value.toLowerCase())).slice(0,25))
             }
             case "width":
             case "height": {
