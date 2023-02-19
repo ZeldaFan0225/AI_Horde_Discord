cf94106fe61348562be5bcb03effd9ed6f6d2ac4
ZeldaFan0225 <78901316+ZeldaFan0225@users.noreply.github.com>
2023-02-19 13:02:51 +0100

    fix wrong error message

--- src/commands/advanced_generate.ts
+++ src/commands/advanced_generate.ts
@@ -254,7 +254,7 @@ export default class extends Command {
         const can_bypass = ctx.client.config.advanced_generate?.source_image?.whitelist?.bypass_checks && ctx.client.config.advanced_generate?.source_image?.whitelist?.user_ids?.includes(ctx.interaction.user.id)
         const party = await ctx.client.getParty(ctx.interaction.channelId, ctx.database)
 
-        if(party?.channel_id) return ctx.error({error: `You can only use ${ctx.client.getSlashCommandTag("generate")} in parties`, codeblock: false})
+        if(party?.channel_id) return ctx.error({error: `You can only use ${await ctx.client.getSlashCommandTag("generate")} in parties`, codeblock: false})
         if(ctx.client.config.advanced_generate?.require_login && !user_token) return ctx.error({error: `You are required to ${await ctx.client.getSlashCommandTag("login")} to use ${await ctx.client.getSlashCommandTag("advanced_generate")}`, codeblock: false})
         if(ctx.client.config.advanced_generate?.blacklisted_words?.some(w => prompt.toLowerCase().includes(w.toLowerCase()))) return ctx.error({error: "Your prompt included one or more blacklisted words"})
         if(height % 64 !== 0) return ctx.error({error: "Height must be a multiple of 64"})
